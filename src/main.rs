511529fab51bfc04b22f0e8b0c24e9ef12ea2750
SamuelVanie <samuelmichaelvanie@gmail.com>
2024-02-06 00:33:35 +0100

    feat: broadcast message to all clients from one client

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,42 @@
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    net::TcpListener,
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener, sync::broadcast,
 };
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    let (tx, _rx) = broadcast::channel::<String>(10);
+
     loop {
         let (mut socket, _addr) = listener.accept().await.unwrap();
+
+        let tx = tx.clone();
+
+        let mut rx = tx.subscribe();
+
         tokio::spawn(async move {
-            let mut buffer = [0u8; 1024];
+            let (reader, mut writer) = socket.split();
+
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
             loop {
-                let bytes_read = socket.read(&mut buffer).await.unwrap();
-                socket.write_all(&buffer[..bytes_read]).await.unwrap();
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                        tx.send(line.clone()).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let msg = result.unwrap();
+                        writer.write_all(msg.as_bytes()).await.unwrap();
+                    }
+                }
             }
         });
     }
