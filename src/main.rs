098d01f5f0de285b85db373a9dd60303e6dec4cd
Samuel Michael Vanie <samuelmichaelvanie@Samuels-MacBook-Air.local>
2024-02-05 07:00:22 +0100

    feat: simple echo server
    - The server is able to handle multiple clients now
    - The server echo back what the user send
    - Each client is handle as a worker

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,14 @@ use tokio::{
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-
     loop {
-        let mut buffer = [0u8; 1024];
-        let bytes_read = socket.read(&mut buffer).await.unwrap();
-
-        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            let mut buffer = [0u8; 1024];
+            loop {
+                let bytes_read = socket.read(&mut buffer).await.unwrap();
+                socket.write_all(&buffer[..bytes_read]).await.unwrap();
+            }
+        });
     }
 }
